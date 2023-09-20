767b33301c90eb278f27ea0f69f139930102ef97
Rodolphe Pineau <pineau@rti-zone.org>
2023-09-20 09:25:57 -0700

    X2 clarity II file X2 plugin.
    This Plugin allows TheSkyX to use a Clarity II file with
    some extensions like SQM added at the end of the standard
    1 line Boltwood/Clarity file structure.

---
+++ ClarityIIPlus.h
@@ -0,0 +1,158 @@
+//
+//  ClarityIIPlus.h
+//  CClarityIIPlus
+//
+//  Created by Rodolphe Pineau on 2021-04-13
+//  ClarityIIPlus X2 plugin
+
+#ifndef __ClarityIIPlus__
+#define __ClarityIIPlus__
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <memory.h>
+
+#ifdef SB_MAC_BUILD
+#include <unistd.h>
+#endif
+
+#ifdef SB_WIN_BUILD
+#include <time.h>
+#endif
+
+#include <math.h>
+#include <string.h>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <chrono>
+#include <thread>
+#include <ctime>
+#include <cmath>
+#include <future>
+#include <mutex>
+
+
+#include "../../licensedinterfaces/sberrorx.h"
+
+#include "StopWatch.h"
+
+
+#define PLUGIN_VERSION      1.0
+
+// #define PLUGIN_DEBUG 3
+
+#define FILE_CHECK_INTERVAL 10  // in seconds
+#define inHg_to_mBar  33.86389
+
+// error codes
+enum ClarityIIPlusErrors {PLUGIN_OK=0, NOT_CONNECTED, CANT_CONNECT, BAD_CMD_RESPONSE, COMMAND_FAILED, COMMAND_TIMEOUT, PARSE_FAILED};
+
+enum ClarityIIPlusWindUnits {KPH=0, MPS, MPH};
+enum ClarityIIPlusTempUnits {CEL=0, FAR, KEL};
+
+class CClarityIIPlus
+{
+public:
+    CClarityIIPlus();
+    ~CClarityIIPlus();
+
+    int         Connect();
+    void        Disconnect(void);
+    bool        IsConnected(void) { return m_bIsConnected; }
+    void        getFirmware(std::string &sFirmware);
+
+
+    int         getData();
+
+    void getClarityIIDataFileName(std::string &fName);
+    void setClarityIIDataFileName(std::string fName);
+
+    int     getTempUnit();
+    int     getWindSpeedUnit();
+    double  getSkyTemp();
+    double  getAmbientTemp();
+    double  getSensorTemp();
+    double  getWindSpeed();
+    double  getHumidity();
+    double  getDewPointTemp();
+    int     getHeaterPower();
+    int     getRainFlag();
+    int     getWetlag();
+    int     getTimeSinceGoodData();
+
+    int     getCloudCondition();
+    int     getWindCondition();
+    int     getRainCondition();
+    int     getLightCondition();
+    bool    getNeedClose();
+    bool    getAlert();
+    double  getSQM();
+    
+    bool    isSqmAvailable();
+
+#ifdef PLUGIN_DEBUG
+    void  log(const std::string sLogLine);
+#endif
+
+protected:
+
+    int             readDataFile();
+
+    bool            m_bIsConnected;
+
+    // ClarityIIPlus variables
+    // "New format" with SQM appended:
+    // Date       Time        T V   SkyT   AmbT   SenT   Wind Hum  DewPt Hea R W Since Now()Day's   c w r d C A   SQM
+    // 2005-06-03 02:07:23.34 C K  -28.5   18.7   22.5   45.3  75   10.3   3 0 0 00004 038506.08846 1 2 1 0 0 0 +21.88
+    // 012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890
+    // 0         1         2         3         4         5         6         7         8         9        10        11
+
+    int                 m_nTempUnit;
+    int                 m_nWinSpeedUnit;
+    std::atomic<double> m_dSkyTemp;
+    std::atomic<double> m_dTemp;
+    std::atomic<double> m_dSensorTemp;
+    std::atomic<double> m_dWindSpeed;
+    std::atomic<double> m_dPercentHumdity;
+    std::atomic<double> m_dDewPointTemp;
+    std::atomic<int>    m_nheaterPower;
+    
+    std::atomic<int>    m_nRainFlag;
+    std::atomic<int>    m_nWetFlag;
+    std::atomic<int>    m_nGoodDataSince;
+    
+    // std::atomic<double> m_dBarometricPressure;
+    std::atomic<int>    m_nCloudCondition;
+    std::atomic<int>    m_nWindCondition;
+    std::atomic<int>    m_nRainCondition;
+    std::atomic<int>    m_nDaylightCondition;
+    std::atomic<bool>   m_bNeedClose;
+    std::atomic<bool>   m_bAlert;
+    std::atomic<double> m_dSQM;
+
+    std::atomic<bool>   m_bSafe;
+    std::atomic<bool>   m_bHasSqm;
+    
+    std::string     m_sDataFilePath;
+    std::ifstream   m_Datafile;
+    std::string     m_sClarityIIData;
+    std::string&    trim(std::string &str, const std::string &filter );
+    std::string&    ltrim(std::string &str, const std::string &filter);
+    std::string&    rtrim(std::string &str, const std::string &filter);
+
+    
+#ifdef PLUGIN_DEBUG
+    // timestamp for logs
+    const std::string getTimeStamp();
+    std::ofstream m_sLogFile;
+    std::string m_sLogfilePath;
+    std::string m_sPlatform;
+#endif
+
+};
+
+#endif
---
+++ x2weatherstation.cpp
@@ -0,0 +1,376 @@
+
+#include "x2weatherstation.h"
+
+X2WeatherStation::X2WeatherStation(const char* pszDisplayName,
+												const int& nInstanceIndex,
+												SerXInterface						* pSerXIn,
+												TheSkyXFacadeForDriversInterface	* pTheSkyXIn,
+												SleeperInterface					* pSleeperIn,
+												BasicIniUtilInterface				* pIniUtilIn,
+												LoggerInterface						* pLoggerIn,
+												MutexInterface						* pIOMutexIn,
+												TickCountInterface					* pTickCountIn)
+
+{
+	m_pSerX							= pSerXIn;
+	m_pTheSkyXForMounts				= pTheSkyXIn;
+	m_pSleeper						= pSleeperIn;
+	m_pIniUtil						= pIniUtilIn;
+	m_pLogger						= pLoggerIn;
+	m_pIOMutex						= pIOMutexIn;
+	m_pTickCount					= pTickCountIn;
+    m_nPrivateISIndex               = nInstanceIndex;
+
+    char szFname[2048];
+    std::string sFname;
+    
+	m_bLinked = false;
+    m_bUiEnabled = false;
+
+    if (m_pIniUtil) {
+        m_pIniUtil->readString(PARENT_KEY, CHILD_KEY_FNAME, "", szFname, 2048);
+        sFname.assign(szFname);
+        m_ClarityIIPlus.setClarityIIDataFileName(sFname);
+        m_dSqmThreshold = m_pIniUtil->readDouble(PARENT_KEY, CHILD_KEY_SQM, 13);
+    }
+    m_DataTimer.Reset();
+}
+
+X2WeatherStation::~X2WeatherStation()
+{
+	//Delete objects used through composition
+	if (GetSerX())
+		delete GetSerX();
+	if (GetTheSkyXFacadeForDrivers())
+		delete GetTheSkyXFacadeForDrivers();
+	if (GetSleeper())
+		delete GetSleeper();
+	if (GetSimpleIniUtil())
+		delete GetSimpleIniUtil();
+	if (GetLogger())
+		delete GetLogger();
+	if (GetMutex())
+		delete GetMutex();
+}
+
+int	X2WeatherStation::queryAbstraction(const char* pszName, void** ppVal)
+{
+	*ppVal = NULL;
+
+	if (!strcmp(pszName, LinkInterface_Name))
+		*ppVal = (LinkInterface*)this;
+	else if (!strcmp(pszName, WeatherStationDataInterface_Name))
+        *ppVal = dynamic_cast<WeatherStationDataInterface*>(this);
+    else if (!strcmp(pszName, ModalSettingsDialogInterface_Name))
+        *ppVal = dynamic_cast<ModalSettingsDialogInterface*>(this);
+    else if (!strcmp(pszName, X2GUIEventInterface_Name))
+        *ppVal = dynamic_cast<X2GUIEventInterface*>(this);
+
+
+	return SB_OK;
+}
+
+int X2WeatherStation::execModalSettingsDialog()
+{
+    int nErr = SB_OK;
+    X2ModalUIUtil uiutil(this, GetTheSkyXFacadeForDrivers());
+    X2GUIInterface*                    ui = uiutil.X2UI();
+    X2GUIExchangeInterface*            dx = NULL;//Comes after ui is loaded
+    bool bPressedOK = false;
+    std::stringstream ssTmp;
+    std::string sTmp;
+    char szTmpBuf[4096];
+    
+    m_bUiEnabled = false;
+
+    if (NULL == ui)
+        return ERR_POINTER;
+    if ((nErr = ui->loadUserInterface("ClarityIIPlus.ui", deviceType(), m_nPrivateISIndex))) {
+        return nErr;
+    }
+
+    if (NULL == (dx = uiutil.X2DX())) {
+        return ERR_POINTER;
+    }
+    X2MutexLocker ml(GetMutex());
+
+    m_ClarityIIPlus.getClarityIIDataFileName(sTmp);
+    dx->setText("filePath", sTmp.c_str());
+    if(sTmp.size())
+        m_ClarityIIPlus.getData();
+
+    dx->setEnabled("sqmThreshold", m_ClarityIIPlus.isSqmAvailable());
+
+    updateUI(dx);
+    dx->setPropertyDouble("sqmThreshold", "value", m_dSqmThreshold);
+    
+    m_bUiEnabled = true;
+
+    //Display the user interface
+    if ((nErr = ui->exec(bPressedOK)))
+        return nErr;
+
+    //Retreive values from the user interface
+    if (bPressedOK) {
+        dx->propertyString("filePath", "text", szTmpBuf, 4095);
+        sTmp.assign(szTmpBuf);
+        m_ClarityIIPlus.setClarityIIDataFileName(sTmp);
+        m_pIniUtil->writeString(PARENT_KEY, CHILD_KEY_FNAME, sTmp.c_str());
+        dx->propertyDouble("sqmThreshold", "value", m_dSqmThreshold);
+        m_pIniUtil->writeDouble(PARENT_KEY, CHILD_KEY_SQM, m_dSqmThreshold);
+    }
+    return nErr;
+}
+
+void X2WeatherStation::uiEvent(X2GUIExchangeInterface* uiex, const char* pszEvent)
+{
+    int nErr = PLUGIN_OK;
+    std::stringstream ssTmp;
+    std::string sTmp;
+    char szTmpBuf[4096];
+    
+    // the test for m_bUiEnabled is done because even if the UI is not displayed we get events on the comboBox changes when we fill it.
+    if(!m_bUiEnabled)
+        return;
+
+
+    if (!strcmp(pszEvent, "on_timer")) {
+        m_ClarityIIPlus.getData();
+        updateUI(uiex);
+    }
+    else if (!strcmp(pszEvent, "on_pushButton_clicked")) {
+        uiex->propertyString("filePath", "text", szTmpBuf, 4095);
+        sTmp.assign(szTmpBuf);
+        m_ClarityIIPlus.setClarityIIDataFileName(sTmp);
+        nErr = m_ClarityIIPlus.getData();
+        if (nErr) {
+            ssTmp << "Read file error : " << nErr;
+            uiex->messageBox("Error reading file", ssTmp.str().c_str());
+            return;
+        }
+        uiex->setEnabled("sqmThreshold", m_ClarityIIPlus.isSqmAvailable());
+        updateUI(uiex);
+    }
+}
+
+void X2WeatherStation::updateUI(X2GUIExchangeInterface* dx)
+{
+    std::stringstream ssTmp;
+    double dTmp;
+    int nTmp;
+
+    dTmp = m_ClarityIIPlus.getSkyTemp();
+    ssTmp << std::fixed << std::setprecision(2) << dTmp << "ºC";
+    dx->setText("skyTemp", ssTmp.str().c_str());
+    std::stringstream().swap(ssTmp);
+
+    dTmp = m_ClarityIIPlus.getAmbientTemp();
+    ssTmp << std::fixed << std::setprecision(2) << dTmp << "ºC";
+    dx->setText("ambientTemp", ssTmp.str().c_str());
+    std::stringstream().swap(ssTmp);
+
+    dTmp = m_ClarityIIPlus.getSensorTemp();
+    ssTmp << std::fixed << std::setprecision(2) << dTmp << "ºC";
+    dx->setText("sensorTemp", ssTmp.str().c_str());
+    std::stringstream().swap(ssTmp);
+
+    dTmp = m_ClarityIIPlus.getWindSpeed();
+    nTmp = m_ClarityIIPlus.getWindSpeedUnit();
+    std::stringstream().swap(ssTmp);
+    ssTmp << std::fixed << std::setprecision(2) << dTmp << (nTmp==KPH?" Km/h":" MPH");
+    dx->setText("windSpeed", ssTmp.str().c_str());
+    std::stringstream().swap(ssTmp);
+
+    dTmp = m_ClarityIIPlus.getHumidity();
+    ssTmp << std::fixed << std::setprecision(2) << dTmp << "%";
+    dx->setText("humidity", ssTmp.str().c_str());
+    std::stringstream().swap(ssTmp);
+
+    dTmp = m_ClarityIIPlus.getDewPointTemp();
+    ssTmp << std::fixed << std::setprecision(2) << dTmp << "ºC";
+    dx->setText("dewPoint", ssTmp.str().c_str());
+    std::stringstream().swap(ssTmp);
+
+    if(m_ClarityIIPlus.isSqmAvailable()) {
+        dTmp = m_ClarityIIPlus.getSQM();
+        ssTmp << std::fixed << std::setprecision(2) << dTmp << " mpsas";
+        dx->setText("SQM", ssTmp.str().c_str());
+    }
+    else {
+        dx->setText("SQM", "N/A");
+
+    }
+}
+
+void X2WeatherStation::driverInfoDetailedInfo(BasicStringInterface& str) const
+{
+    str = "ClarityII Plus X2 plugin for Boltwood and AAG-CloudWatcher enhanced Clarity II file by Rodolphe Pineau";
+}
+
+double X2WeatherStation::driverInfoVersion(void) const
+{
+	return PLUGIN_VERSION;
+}
+
+void X2WeatherStation::deviceInfoNameShort(BasicStringInterface& str) const
+{
+    str = "ClarityII Plus";
+}
+
+void X2WeatherStation::deviceInfoNameLong(BasicStringInterface& str) const
+{
+    deviceInfoNameShort(str);
+}
+
+void X2WeatherStation::deviceInfoDetailedDescription(BasicStringInterface& str) const
+{
+    deviceInfoNameShort(str);
+}
+
+void X2WeatherStation::deviceInfoFirmwareVersion(BasicStringInterface& str)
+{
+    if(m_bLinked) {
+        str = "N/A";
+        std::string sFirmware;
+        X2MutexLocker ml(GetMutex());
+        m_ClarityIIPlus.getFirmware(sFirmware);
+        str = sFirmware.c_str();
+    }
+    else
+        str = "N/A";
+
+}
+
+void X2WeatherStation::deviceInfoModel(BasicStringInterface& str)
+{
+    deviceInfoNameShort(str);
+}
+
+int	X2WeatherStation::establishLink(void)
+{
+    int nErr = SB_OK;
+
+    X2MutexLocker ml(GetMutex());
+    nErr = m_ClarityIIPlus.Connect();
+    if(nErr)
+        m_bLinked = false;
+    else
+        m_bLinked = true;
+
+	return nErr;
+}
+int	X2WeatherStation::terminateLink(void)
+{
+    m_ClarityIIPlus.Disconnect();
+
+	m_bLinked = false;
+	return SB_OK;
+}
+
+
+bool X2WeatherStation::isLinked(void) const
+{
+	return m_bLinked;
+}
+
+
+int X2WeatherStation::weatherStationData(double& dSkyTemp,
+                                         double& dAmbTemp,
+                                         double& dSenT,
+                                         double& dWind,
+                                         int& nPercentHumdity,
+                                         double& dDewPointTemp,
+                                         int& nRainHeaterPercentPower,
+                                         int& nRainFlag,
+                                         int& nWetFlag,
+                                         int& nSecondsSinceGoodData,
+                                         double& dVBNow,
+                                         double& dBarometricPressure,
+                                         WeatherStationDataInterface::x2CloudCond& cloudCondition,
+                                         WeatherStationDataInterface::x2WindCond& windCondition,
+                                         WeatherStationDataInterface::x2RainCond& rainCondition,
+                                         WeatherStationDataInterface::x2DayCond& daylightCondition,
+                                         int& nRoofCloseThisCycle //The weather station hardware determined close or not (boltwood hardware says cloudy is not close)
+)
+{
+    int nErr = SB_OK;
+    int dWindCond;
+    int nTempUnit;
+    double dSqm;
+    
+    if(!m_bLinked)
+        return ERR_NOLINK;
+
+    X2MutexLocker ml(GetMutex());
+    if(m_DataTimer.GetElapsedSeconds() > 5 ) {
+        m_ClarityIIPlus.getData();
+        m_DataTimer.Reset();
+    }
+    
+    nSecondsSinceGoodData = m_ClarityIIPlus.getTimeSinceGoodData();
+    dSkyTemp = m_ClarityIIPlus.getSkyTemp();
+    dAmbTemp = m_ClarityIIPlus.getAmbientTemp();
+    nTempUnit = m_ClarityIIPlus.getTempUnit();
+    if(nTempUnit == FAR) {
+        // convert to C
+        dSkyTemp = (dSkyTemp-32) / 1.8;
+        dAmbTemp = (dAmbTemp-32) / 1.8;
+    }
+    else if(nTempUnit == KEL) {
+        dSkyTemp = dSkyTemp-273.15;
+        dAmbTemp = dAmbTemp-273.15;
+    }
+    
+    dSenT = m_ClarityIIPlus.getSensorTemp();
+    dWind = m_ClarityIIPlus.getWindSpeed();
+	nPercentHumdity = int(m_ClarityIIPlus.getHumidity());
+	dDewPointTemp = m_ClarityIIPlus.getDewPointTemp();
+    nRainHeaterPercentPower = m_ClarityIIPlus.getHeaterPower();
+	nRainFlag = m_ClarityIIPlus.getRainFlag();
+	nWetFlag = m_ClarityIIPlus.getWetlag();
+
+    dWindCond = m_ClarityIIPlus.getWindCondition();
+
+    cloudCondition = (WeatherStationDataInterface::x2CloudCond)m_ClarityIIPlus.getCloudCondition();
+    windCondition = (WeatherStationDataInterface::x2WindCond)m_ClarityIIPlus.getWindCondition();
+    rainCondition = (WeatherStationDataInterface::x2RainCond)m_ClarityIIPlus.getRainCondition();
+    if(m_ClarityIIPlus.isSqmAvailable()) {
+        dSqm = m_ClarityIIPlus.getSQM();
+        if(dSqm >= m_dSqmThreshold) {
+            daylightCondition = WeatherStationDataInterface::x2DayCond::dayDark;
+        }
+        else {
+            daylightCondition = WeatherStationDataInterface::x2DayCond::dayLight;
+        }
+    }
+    else
+        daylightCondition = (WeatherStationDataInterface::x2DayCond)m_ClarityIIPlus.getLightCondition();
+
+    nRoofCloseThisCycle = m_ClarityIIPlus.getNeedClose();
+    
+    
+	return nErr;
+}
+
+WeatherStationDataInterface::x2WindSpeedUnit X2WeatherStation::windSpeedUnit()
+{
+    WeatherStationDataInterface::x2WindSpeedUnit nUnit = WeatherStationDataInterface::x2WindSpeedUnit::windSpeedKph;
+    int ClarityIIPlusUnit;
+    std::stringstream tmp;
+
+    ClarityIIPlusUnit = m_ClarityIIPlus.getWindSpeedUnit();
+
+    switch(ClarityIIPlusUnit) {
+        case KPH:
+            nUnit = WeatherStationDataInterface::x2WindSpeedUnit::windSpeedKph;
+            break;
+        case MPS:
+            nUnit = WeatherStationDataInterface::x2WindSpeedUnit::windSpeedMps;
+            break;
+        case MPH:
+            nUnit = WeatherStationDataInterface::x2WindSpeedUnit::windSpeedMph;
+            break;
+    }
+
+    return nUnit ;
+}
